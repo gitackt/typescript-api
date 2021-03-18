f7c35ed8e4e045514e510ef2723b49630719dc29
gitackt <picos.tackt@gmail.com>
2021-03-18 23:22:46 +0900

    Fix

--- scripts/openapi.sh
+++ scripts/openapi.sh
@@ -1,1 +1,1 @@
-openapi-generator-cli generate -g typescript-axios -i ./openapi.yml -o ./api/src/interfaces
+openapi-generator generate -g typescript-axios -i ./openapi.yml -o ./api/src/interfaces --additional-properties=modelPropertyNaming=camelCase,supportsES6=true,withInterfaces=true,typescriptThreePlus=true
