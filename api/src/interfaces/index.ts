a00eb32fc8631cf215d900efaa009a33312ee39e
gitackt <picos.tackt@gmail.com>
2021-03-18 23:04:03 +0900

    Add types

--- api/src/interfaces/index.ts
+++ api/src/interfaces/index.ts
@@ -1,4 +1,5 @@
-// tslint:disable
+/* tslint:disable */
+/* eslint-disable */
 /**
  * typescript API
  * typescript-api
@@ -14,3 +15,4 @@
 
 export * from "./api";
 export * from "./configuration";
+
