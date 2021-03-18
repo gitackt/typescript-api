503a95056919931d53c587d3119233e26ea7c87d
gitackt <picos.tackt@gmail.com>
2021-03-18 23:13:27 +0900

    Change script name

--- scripts/restart.sh
+++ scripts/restart.sh
@@ -2,5 +2,5 @@ docker-compose down -v
 docker-compose build --parallel
 docker-compose up -d
 sleep 20
-sh scripts/db_migration_run.sh
-sh scripts/db_seed_run.sh
+sh scripts/migrate.sh
+sh scripts/seed.sh
