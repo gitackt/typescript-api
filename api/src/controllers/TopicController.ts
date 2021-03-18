2114e7288fddb4944ef2294057d9dd7f020407f9
gitackt <picos.tackt@gmail.com>
2021-03-18 23:41:58 +0900

    Fix comtroller

--- api/src/controllers/TopicController.ts
+++ api/src/controllers/TopicController.ts
@@ -1,15 +1,70 @@
 import { Request, Response } from 'express'
+import httpContext from 'express-http-context'
+
 import { TopicService } from '../domain/service/TopicService'
+import { Topic } from '../interfaces/api'
+import { Topic as TopicModel } from '../domain/models/Topic'
 
 export class TopicController {
+  private readonly topicService: TopicService
+
+  constructor(topicService: TopicService) {
+    this.topicService = topicService
+  }
+
   async topics(req: Request, res: Response) {
-    const topics = await new TopicService().getTopics()
-    res.status(200).send(topics)
+    const order = req.query.order as 'ASC' | 'DESC' | undefined
+    const limit = req.query.limit as number | undefined
+    const offset = req.query.offset as number | undefined
+    const topics = await this.topicService.getTopics(order, limit, offset)
+    res.status(200).send(topics.map(topic => topicConverter(topic)))
   }
 
   async topic(req: Request, res: Response) {
-    const id = req.params.id
-    const topic = await new TopicService().getTopic(id)
+    const topic = await this.topicService.getTopic(Number(req.params.id))
+    if (!topic) {
+      res.status(400).send({ message: 'not found' })
+      return
+    }
+    res.status(200).send(topicConverter(topic))
+  }
+
+  async createTopic(req: Request, res: Response) {
+    const body = req.body as Topic
+    const userId = httpContext.get('userId')
+    const topic = await this.topicService.createTopic(topicModelConverter(body), userId)
     res.status(200).send(topic)
   }
+
+  async updateTopic(req: Request, res: Response) {
+    const body = req.body as Topic
+    const userId = httpContext.get('userId')
+    const topic = await this.topicService.updateTopic(topicModelConverter(body), userId)
+    if (!topic) {
+      res.status(400).send({ message: 'not found' })
+      return
+    }
+    res.status(200).send(topicConverter(topic))
+  }
+}
+
+const topicConverter = (model: TopicModel): Topic => {
+  const response: Topic = {
+    id: model.id || 0,
+    title: model.title || '',
+    content: model.content || '',
+    createdAt: model.createdAt ? model.createdAt.toISOString() : '',
+    updatedAt: model.updatedAt ? model.updatedAt.toISOString() : '',
+  }
+  return response
+}
+
+const topicModelConverter = (topic: Topic): TopicModel => {
+  const response: TopicModel = {
+    title: topic.title || '',
+    content: topic.content || '',
+    createdAt: topic.createdAt ? new Date(topic.createdAt) : undefined,
+    updatedAt: topic.updatedAt ? new Date(topic.updatedAt) : undefined,
+  }
+  return response
 }
--- api/src/controllers/UserController.ts
+++ api/src/controllers/UserController.ts
@@ -1,15 +1,57 @@
 import { Request, Response } from 'express'
-import { UserService } from '../../domain/service/UserService'
+import httpContext from 'express-http-context'
+
+import { User } from '../interfaces/api'
+import { User as UserModel } from '../domain/models/User'
+import { UserService } from '../domain/service/UserService'
 
 export class UserController {
-  async users(req: Request, res: Response) {
-    const users = await new UserService().getUsers()
-    res.status(200).send(users)
+  private readonly userService: UserService
+
+  constructor(userService: UserService) {
+    this.userService = userService
   }
 
   async user(req: Request, res: Response) {
-    const id = req.params.id
-    const user = await new UserService().getUser(id)
-    res.status(200).send(user)
+    const userId = httpContext.get('userId')
+    const user = await this.userService.getUser(userId)
+    if (!user) {
+      res.status(400).send({ message: 'not found' })
+      return
+    }
+    res.status(200).send(userConverter(user))
   }
+
+  async updateUser(req: Request, res: Response) {
+    const body = req.body as User
+    const userId = httpContext.get('userId')
+    if (userId !== body.id) {
+      res.status(400).send({ message: 'invalid userId' })
+      return
+    }
+    const user = await this.userService.updateUser(userModelConverter(body))
+    if (!user) {
+      res.status(400).send({ message: 'not found' })
+      return
+    }
+    res.status(200).send(userConverter(user))
+  }
+}
+
+const userConverter = (model: UserModel): User => {
+  const response: User = {
+    id: model.id || 0,
+    createdAt: model.createdAt ? model.createdAt.toISOString() : '',
+    updatedAt: model.updatedAt ? model.updatedAt.toISOString() : '',
+  }
+  return response
+}
+
+const userModelConverter = (user: User): UserModel => {
+  const response: UserModel = {
+    id: user.id,
+    createdAt: user.createdAt ? new Date(user.createdAt) : undefined,
+    updatedAt: user.updatedAt ? new Date(user.updatedAt) : undefined,
+  }
+  return response
 }
